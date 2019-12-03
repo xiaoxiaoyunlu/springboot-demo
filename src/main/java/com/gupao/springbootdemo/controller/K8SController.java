5d92a5fbe2a5f7a51b6c7f6fe582a104863a4968
xiaoxiaoyunlu <zjxzz0714@163.com>
2019-12-03 16:06:48 +0800

    Default Changelist

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -17,7 +17,7 @@ public class K8SController {
             InetAddress address = InetAddress.getLocalHost();
             result="hostname: "+address.getHostName()+"hostaddress: "+address.getHostAddress();
             System.out.println("hostname:"+address.getHostName());//主机名
-            System.out.println();//主机别名
+            System.out.println("1111111111111");//主机别名
             System.out.println("test successfully");//
         }catch(Exception e){
             e.printStackTrace();
